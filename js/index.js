82a8c79aafe30b966071e7d2680556585bd84af4
ooluwaseunmicheal <ooluwaseunmicheal@gmail.com>
2020-07-23 16:27:59 +0100

    updated

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function currentLink () {
+    const curPage = document.URL;
+    const links = document.getElementByTagName ('a');
+    for (let of links) {
+        if (links.href == curPage) {
+            link.classList.add ("active");
+
+        }
+    }
+}
+
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        currentLink()
+    }
+};
