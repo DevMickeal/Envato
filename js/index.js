331e381df4b88e2ab5a61f827e932bf92bd8db62
ooluwaseunmicheal <ooluwaseunmicheal@gmail.com>
2020-07-25 17:36:44 +0100

    final

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-function currentLink () {
-    const curPage = document.URL;
-    const links = document.getElementByTagName ('a');
-    for (let of links) {
-        if (links.href == curPage) {
-            link.classList.add ("active");
-
-        }
+function openLink(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-}
-
-document.onreadystatechange = () => {
-    if (document.readyState === 'complete') {
-        currentLink()
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-};
-
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
